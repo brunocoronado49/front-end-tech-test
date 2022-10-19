7480a2a74054e0de61b0f9e828a2675181ad67de
Bruno Coronado <brunocoronado@MacBook-Air-de-Bruno.local>
2022-10-19 10:49:54 -0500

    Subiendo ultimos cambios para hacer un proyecto desde 0

--- front/src/App.js
+++ front/src/App.js
@@ -1,35 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import CartPage from "./components/CartPage";
-// import ProductDetail from "./components/ProductDetail";
-// import ProductList from "./components/ProductList";
-import "./App.css";
+import CartPage from "./components/CartPage";
+import ProductDetail from "./components/ProductDetail";
+import ProductList from "./components/ProductList";
+import { useNavigate } from 'react-router-dom'
 
 const App = () => {
+    const navigate = useNavigate();
+    
     return (
         <div className="app">
-            {/* <Navbar />
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<ProductList />} />
-                    <Route path="/cart" element={<CartPage />} />
-                    <Route
-                        path="/product/detail/:id"
-                        element={<ProductDetail />}
-                    />
-                </Routes>
-            </BrowserRouter> */}
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<HelloWorld />} />
-                </Routes>
-            </BrowserRouter>
+            <ProductList />
+            
         </div>
     );
-};
-
-const HelloWorld = () => {
-    return <h1>Hello World</h1>;
 };
 
 export default App;
